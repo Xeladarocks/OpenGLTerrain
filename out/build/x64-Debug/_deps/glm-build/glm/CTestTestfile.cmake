b176cb95a2a122d42d5256989db5aa3bbddfde10
xelada <alexdalat@gmail.com>
2020-05-18 15:33:23 -0400

    update

---
+++ out/build/x64-Debug/_deps/glm-build/glm/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/dev/c++/OpenGLCMake/out/build/x64-Debug/extern/glm/glm
+# Build directory: C:/dev/c++/OpenGLCMake/out/build/x64-Debug/_deps/glm-build/glm
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
